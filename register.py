85edafaa2df060fce3a03945559edfafd31ffc2f
Otabek Rizayev <46789195+Otabek-Rizayev@users.noreply.github.com>
2022-01-08 14:42:49 +0500

    100% work register

--- register.py
+++ register.py
@@ -7,17 +7,17 @@ while fal:
     if a.new_login():
         password = a.new_password()
     else:
-        print("Login faqat harflardan iborat bo'lsin!")
+        print("Login faqat harflardan iborat bo`lsin!")
         continue
     password2 = a.confirm_password()
-    if password.isdigit() and password2.isdigit():
+    if password.isalpha() and password2.isalpha():
         print("Parol faqat raqamlardan iborat bo`lsin!")
     elif password == password2:
         a.data_base()
         print("Done!")
         fal = False
     else:
-        print("Did not confirm password")
+        print("Parol tasdiqlanmadi!")
 
 
     
